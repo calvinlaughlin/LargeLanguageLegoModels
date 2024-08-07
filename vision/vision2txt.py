1ea0793dea98121fb881c96be73717357794cb48
Calvin Laughlin <laughlincalvin@gmail.com>
2024-08-07 10:10:23 -0700

    Organized repo

--- vision/vision2txt.py
+++ vision/vision2txt.py
@@ -5,40 +5,32 @@ import os
 import re
 
 # OpenAI API Key
-api_key = "sk-proj-HMzMuOfGEJwmH58C8p2AT3BlbkFJOnzsQdIok4MngMnPNE4M"
+api_key = "sk-proj-ICXYnrO45vlQuUtjQK8vT3BlbkFJ0t74jmpKqIMq1Fqawd2Q"
 
-# Function to encode the image
 def encode_image(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode('utf-8')
 
-# Path to the folder
-folder_path = '/Users/alexwang/Desktop/CS224NLego/lego_instructions/6474914.png'
+folder_path = '/Users/alexwang/Desktop/CS224NLego/lego_instructions/6462457.png'
 
-# List to store file names
 # image_paths = ["/Users/alexwang/Desktop/CS224NLego/lego_instructions/6128668.png/3faca61e7dc14080a4006e3deea3d9dedYSwG78XsOHJS4MW-6.png"]
 image_paths = []
 
 
-# Function to extract the numerical value following the '-' character
 def extract_number_after_dash(file_path):
     file_name = os.path.basename(file_path)
     match = re.search(r'-(\d+)', file_name)
     return int(match.group(1)) if match else float('inf')  # Use 'inf' to handle filenames without the pattern
 
-# Iterate over all files in the folder
 for file_name in os.listdir(folder_path):
     # Check if it is a file (not a directory)
     if os.path.isfile(os.path.join(folder_path, file_name)):
         image_paths.append(os.path.join(folder_path, file_name))
 
-# Sort the image paths based on the extracted number after the dash
 image_paths = sorted(image_paths, key=extract_number_after_dash)
 
-# Initialize an empty list to store results
 results = []
 
-# Iterate through each image path
 for image_path in image_paths:
     # Getting the base64 string
     base64_image = encode_image(image_path)
@@ -87,17 +79,9 @@ for image_path in image_paths:
     results.append(message_content)
 
 
-# Write the results to a text file
-output_file_path = 'lego_instructions_6474914.txt'
+output_file_path = 'lego_instructions_6462457.txt'
 with open(output_file_path, 'w') as file:
     for result in results:
         file.write(result + "\n\n")  # Write each result as a paragraph with a newline in between
 
 print(f"Instructions have been exported to {output_file_path}")
-
-# Print or process the results as needed
-# print(results)
-
-
-# instruction classifications
-# other, instruction, piece dictionary
